58c4522d4d2806ceb4f8b041086ae0cae58b36e0
Gasarayev <gasarayev.emil@gmail.com>
2025-06-24 20:43:33 +0400

    start node js

---
+++ server/data/products.js
@@ -0,0 +1,52 @@
+const movies = [
+  {
+    id: 1,
+    name: "The Shawshank Redemption",
+    price: 4.99,
+    description: "Drama film about hope and friendship inside a prison",
+  },
+  {
+    id: 2,
+    name: "The Godfather",
+    price: 5.5,
+    description: "Classic mafia movie directed by Francis Ford Coppola",
+  },
+  {
+    id: 3,
+    name: "Inception",
+    price: 6.25,
+    description: "Sci-fi thriller exploring the world of dreams",
+  },
+  {
+    id: 4,
+    name: "The Dark Knight",
+    price: 5.0,
+    description: "Batman faces Joker in this action-packed sequel",
+  },
+  {
+    id: 5,
+    name: "Forrest Gump",
+    price: 4.75,
+    description: "Inspiring story of a man who witnesses historical moments",
+  },
+  {
+    id: 6,
+    name: "Interstellar",
+    price: 7.0,
+    description: "Sci-fi epic exploring space travel and time relativity",
+  },
+  {
+    id: 7,
+    name: "Parasite",
+    price: 5.99,
+    description: "Korean thriller-drama about class division",
+  },
+  {
+    id: 8,
+    name: "Coco",
+    price: 3.5,
+    description: "Animated film about family, music, and the Day of the Dead",
+  },
+];
+
+module.exports = movies;
